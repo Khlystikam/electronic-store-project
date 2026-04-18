b36c0b9261760b8e6646cedd1130fbfdabf6496c
A.Khlystik <aleksandr@dev-khlystikam.ru>
2026-04-18 18:50:30 +0000

    reupdate добаления импорт через json

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,28 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 const prismaClientSingleton = () => {
-	// Используем официальный адаптер для любых баз MySQL
-	const adapter = new PrismaMariaDb(process.env.DATABASE_URL as string);
+    const dbUrl = new URL(process.env.DATABASE_URL as string);
 
-	// Передаем адаптер в клиент (как и требовала ошибка!)
-	return new PrismaClient({ adapter });
+    // Передаем настройки напрямую в адаптер (как и просит TypeScript)
+    const adapter = new PrismaMariaDb({
+        host: dbUrl.hostname,
+        port: Number(dbUrl.port) || 3306,
+        user: dbUrl.username,
+        password: dbUrl.password,
+        database: dbUrl.pathname.substring(1),
+        connectionLimit: 5, // Наш спаситель от таймаутов
+    });
+
+    return new PrismaClient({ adapter });
 };
 
 declare const globalThis: {
-	prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
 export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
