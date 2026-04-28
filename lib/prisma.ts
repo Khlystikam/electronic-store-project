a4122c41290ba99616e72aa7073dbfd6a61465e3
A.Khlystik <aleksandr@dev-khlystikam.ru>
2026-04-28 10:54:32 +0000

    Удалил адаптер MariaDB, перешел на нативный Prisma Client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 const prismaClientSingleton = () => {
-    const dbUrl = new URL(process.env.DATABASE_URL as string);
-
-    // Передаем настройки напрямую в адаптер (как и просит TypeScript)
-    const adapter = new PrismaMariaDb({
-        host: dbUrl.hostname,
-        port: Number(dbUrl.port) || 3306,
-        user: dbUrl.username,
-        password: dbUrl.password,
-        database: dbUrl.pathname.substring(1),
-        connectionLimit: 5, // Наш спаситель от таймаутов
-    });
-
-    return new PrismaClient({ adapter });
+    return new PrismaClient();
 };
 
 declare const globalThis: {
